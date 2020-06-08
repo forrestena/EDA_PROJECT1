c46f2fe3793928e9d29f7d84c4eb1db17f724c16
forrestena <60902402+forrestena@users.noreply.github.com>
2020-06-08 01:29:13 +0100

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,41 @@
+
+
+datahpc <- file("household_power_consumption.txt")
+
+##examine data for only the limited period
+hpcex <- read.table(text = grep("^[1,2]/2/2007", readLines(datahpc), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+
+
+
+## Getting full dataset
+hpc <- read.csv("household_power_consumption.txt", header = T, sep = ';', 
+                      na.strings = "?", nrows = 2075259, check.names = F, 
+                      stringsAsFactors = F, comment.char = "", quote = '\"')
+hpc$Date <- as.Date(hpc$Date, format = "%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(hpc, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(hpc)
+
+## Setting dates formate
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+
+
+## Generate Plot 4
+par(mfrow = c(2,2), mar = c(4,4,3,1), oma = c(0,0,0,0))
+with(data, {
+  plot(Global_active_power ~ Datetime, type = "l", 
+       ylab = "Global Active Power", xlab = "")
+  plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
+  plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",
+       xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+  legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 3, 
+         bty = "n",
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power ~ Datetime, type = "l", 
+       ylab = "Global_rective_power", xlab = "datetime")
+})
